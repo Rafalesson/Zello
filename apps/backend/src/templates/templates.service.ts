a6ce7739b5cea8704ea533d5dc545bafe6c81341
Rafalesson <rafaalesson35@gmail.com>
2026-06-15 17:17:14 -0300

    ajuste data

--- apps/backend/src/templates/templates.service.ts
+++ apps/backend/src/templates/templates.service.ts
@@ -108,7 +108,7 @@ const allTemplates = {
                 profissionais na presente data, necessitando de
                 <strong>{{durationInDays}} ({{durationInWords}}) dia(s)</strong> de
                 afastamento de suas atividades laborais/outras, a partir de
-                <strong>02/06/2026</strong>.
+                <strong>14/06/2026</strong>.
               </p>
               <p><strong>Finalidade:</strong> {{purpose}}</p>
               <p><strong>CID-10:</strong> {{cidCode}} - {{cidDescription}}</p>
@@ -116,7 +116,7 @@ const allTemplates = {
           </main>
           <footer class="footer">
             <div>
-              <p>Emitido 02/06/2026 às 09:39:41</p>
+              <p>Emitido 14/06/2026 às 10:14:19</p>
               <p id="footer_p2">
                 Atendimento realizado conforme MP nº 2.200-2/2001,
                 <br />
@@ -250,14 +250,14 @@ const allTemplates = {
               <strong>{{patientCPF}}</strong>, encontra-se sob meus cuidados 
               profissionais, sendo-lhe necessário o afastamento de suas atividades 
               habituais por um período de <strong>{{durationInDays}} 
-              ({{durationInWords}})</strong> dia(s), a contar de <strong>07/06/2026</strong>.
+              ({{durationInWords}})</strong> dia(s), a contar de <strong>{{startDate}}</strong>.
             </p>
             <p class="details-line"><strong>Finalidade:</strong> {{purpose}}</p>
             <p class="details-line"><strong>CID-10:</strong> {{cidCode}} - {{cidDescription}}</p>    
           </main>
           <footer class="footer">
             <div>
-              <p>Emitido em 07/06/2026 às 11:39:41</p>
+              <p>Emitido em {{issueDateTime}}</p>
               <p id="footer_p2">
                 Atendimento realizado conforme MP nº 2.200-2/2001,
                 <br />
